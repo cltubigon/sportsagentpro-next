b6252664247b2736add6807e57fd89949d2c820c
cltubigon <cltubigon@gmail.com>
2024-02-07 04:33:06 +0800

    fixed branch

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -10,7 +10,7 @@ const LoginPage = () => {
     <div>
       <Authentication>
         <Text sx={authStyle.title}>
-          User sign inr
+          User sign in
         </Text>
         <LoginForm />
         <Flex sx={authStyle.signupContainer}>
