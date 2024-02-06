63bc81b6e17dcb4f1bf1b37e569e87f9b61250ca
cltubigon <cltubigon@gmail.com>
2024-02-07 04:20:09 +0800

    test update

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -10,7 +10,7 @@ const LoginPage = () => {
     <div>
       <Authentication>
         <Text sx={authStyle.title}>
-          User sign in
+          User sign inss
         </Text>
         <LoginForm />
         <Flex sx={authStyle.signupContainer}>
